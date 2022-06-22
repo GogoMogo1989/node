54c081606ce0eb4b1e8c65f80516b1be19d2a87d
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-06-22 13:30:35 +0200

    20220622

---
+++ people.js
@@ -0,0 +1,8 @@
+const people = [ "laci", "karcsi", "marcsi", "misi"]
+const ages = [ 20, 32, 54, 54]
+
+console.log(people)
+
+const os = require('os')
+
+console.log(os.platform(), os.homedir())
---
+++ test.js
@@ -0,0 +1,10 @@
+/* const name= "joshi"
+
+console.log("name") */
+
+const great = (name) => {
+    console.log(`hello, ${name}`)
+}
+
+great("mario")
+great("joshi")
