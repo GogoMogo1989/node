2dc384183a2d31c3b1ea82e067f5082fca18d118
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-06-23 10:36:17 +0200

    20220623

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs') ;
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method)
+    
 
     //set header content type
     res.setHeader('content-type', 'text/html')
