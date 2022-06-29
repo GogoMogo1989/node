0273a6ce2e831fabe801a88e8a05501730c19f05
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-06-29 15:48:16 +0200

    20220629

--- app.js
+++ app.js
@@ -1,102 +1,90 @@
-const express = require("express")
-const morgan = require("morgan")
-const mongoose = require("mongoose")
-const Blog = require('./models/blog')
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
-
-//express app
+// express app
 const app = express();
 
-//connect to mongoDB
-const dbURI = 'mongodb+srv://netnina:1234@cluster0.dms2yv8.mongodb.net/node-tuts?retryWrites=true&w=majority'
+// connect to mongodb & listen for requests
+const dbURI = 'mongodb+srv://netnina:1234@cluster0.dms2yv8.mongodb.net/node-tuts?retryWrites=true&w=majority';
 
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
- .then((result) => app.listen(3000))
- .catch((err) => console.log(error))
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
-
-//register view engine
+// register view engine
 app.set('view engine', 'ejs');
 
-//middleware & static files
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true}))
-app.use(morgan('dev'))
-
+// mongoose & mongo tests
 app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new blog',
-        snippet: 'abbout my new blog',
-        body: "more about my new blog"
+  const blog = new Blog({
+    title: 'new blog2',
+    snipper: 'about my new blog',
+    body: 'more about my new blog'
+  })
+
+  blog.save()
+    .then(result => {
+      res.send(result);
     })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
-    blog.save()
-     .then((result) => {
-        res.send(result)
-     })
-     .catch((err) => {
-        console.log(err)
-     });
-})
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
-app.use((req, res, next) => {
-    console.log('new request made: ');
-    console.log('host: ', req.hostname);
-    console.log('path: ', req.path);
-    console.log('method: ', req.method);
-    next()
-})
-
-app.use((req, res, next) => {
-    console.log('in the next middleware');
-    next()
-})
-
+app.get('/single-blog', (req, res) => {
+  Blog.findById("62bc54ef0e502e7ad8b39ed8")
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err); 
+    });
+});
 
 app.get('/', (req, res) => {
- 
-   res.redirect('./blogs')
+  res.redirect('/blogs');
 });
-
-app.get('/blogs', (res, req) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-      res.render('index', { title: 'Home', blogs });
-})
 
 app.get('/about', (req, res) => {
-
-   res.render('about', { title: "About"})
-
+  res.render('about', { title: 'About' });
 });
 
-app.post('/blogs', (req, res) => {
-
-    const blog = new Blog(req.body)
-
-    blog.save()
-        .then((result) => {
-            res.redirect('./blogs')
-        })
-        .catch((err) => {
-            console.log(error)
-        })
-
-})
-
+// blog routes
 app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
 
-    res.render('create', { title: "Create"})
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('index', { blogs: result, title: 'All blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
-})
-
-//404
+// 404 page
 app.use((req, res) => {
-
-    res.status(404).render('404', { title: "404"})
-
-})
+  res.status(404).render('404', { title: '404' });
+});
